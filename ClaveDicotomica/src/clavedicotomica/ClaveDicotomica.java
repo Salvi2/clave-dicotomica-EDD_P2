e8bdefdd51633b8fb6b24865d21f7e45f4a8106c
Salvador <salvador.caceres@correo.unimet.edu.ve.com>
2025-03-11 16:08:52 -0400

    Primer Commit, Hello World.

---
+++ ClaveDicotomica/src/clavedicotomica/ClaveDicotomica.java
@@ -0,0 +1,16 @@
+package clavedicotomica;
+
+/**
+ *
+ * @author Salvador Cáceres & Jesús Delgado
+ */
+public class ClaveDicotomica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
